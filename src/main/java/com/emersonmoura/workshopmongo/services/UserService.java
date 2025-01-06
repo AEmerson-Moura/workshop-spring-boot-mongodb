418dc2abc5a7e5e8d54e74a40e83ec19f4a3fd6e
Emerson Moura <emersonsilver@outlook.com.br>
2025-01-06 20:59:48 -0300

    Getting user by id

--- src/main/java/com/emersonmoura/workshopmongo/services/UserService.java
+++ src/main/java/com/emersonmoura/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.emersonmoura.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.emersonmoura.workshopmongo.domain.User;
 import com.emersonmoura.workshopmongo.repositories.UserRepository;
+import com.emersonmoura.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,5 +19,8 @@ public class UserService {
 	public List<User> findAll(){
 		return repository.findAll();
 	}
-	
+	public User findById(String id) {
+		Optional<User>obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto Não Encontrado"));
+	}
 }
