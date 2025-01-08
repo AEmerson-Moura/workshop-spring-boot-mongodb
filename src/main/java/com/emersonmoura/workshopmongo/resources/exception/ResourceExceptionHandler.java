a87f4e5492c734dfb2e90557dfdb55e146f40b01
Emerson Moura <emersonsilver@outlook.com.br>
2025-01-08 20:17:13 -0300

    insert user with POST

--- src/main/java/com/emersonmoura/workshopmongo/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/emersonmoura/workshopmongo/resources/exception/ResourceExceptionHandler.java
@@ -1,7 +1,5 @@
 package com.emersonmoura.workshopmongo.resources.exception;
 
-import java.net.http.HttpRequest;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
--- src/main/java/com/emersonmoura/workshopmongo/services/UserService.java
+++ src/main/java/com/emersonmoura/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.emersonmoura.workshopmongo.domain.User;
+import com.emersonmoura.workshopmongo.dto.UserDTO;
 import com.emersonmoura.workshopmongo.repositories.UserRepository;
 import com.emersonmoura.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -19,8 +20,16 @@ public class UserService {
 	public List<User> findAll(){
 		return repository.findAll();
 	}
+	
 	public User findById(String id) {
 		Optional<User>obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto Não Encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
